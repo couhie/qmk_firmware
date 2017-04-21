22bb7b3c2364025525c0c3e81d58c91c5420f518
couhie <kohei.hieda@gmail.com>
2017-04-21 10:57:39 +0900

    カッコのキーマップ位置を調整

--- keyboards/ergodox/keymaps/couhie/keymap.c
+++ keyboards/ergodox/keymaps/couhie/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| ~L1  |           | ~L1  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L2  |  Esc | Ctrl | Alt  | LGui |                                       | RGui |   [  |   ]  |  Grv |   L1 |
+ *   | ~L2  |  Esc | Ctrl | Alt  | LGui |                                       | RGui |  Grv |   [  |   ]  |   L1 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Del  | Home |       | PgUp | BkSp   |
@@ -40,23 +40,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,       KC_1,         KC_2,         KC_3,    KC_4,    KC_5,    KC_LEFT,
-        KC_TAB,       KC_Q,         KC_W,         KC_E,    KC_R,    KC_T,    KC_SPC,
-        CTL_T(KC_NO), KC_A,         KC_S,         KC_D,    KC_F,    KC_G,
-        KC_LSFT,      KC_Z,         KC_X,         KC_C,    KC_V,    KC_B,    MO(SYMB),
-        MO(MDIA),     KC_ESC,       CTL_T(KC_NO), KC_LALT, KC_LGUI,
-                                                                     KC_DELT, KC_HOME,
-                                                                              KC_END,
-                                                       CTL_T(KC_NO), KC_SPC,  KC_LGUI,
+        KC_EQL,       KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_LEFT,
+        KC_TAB,       KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    KC_SPC,
+        CTL_T(KC_NO), KC_A,   KC_S,         KC_D,    KC_F,         KC_G,
+        KC_LSFT,      KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    MO(SYMB),
+        MO(MDIA),     KC_ESC, CTL_T(KC_NO), KC_LALT, KC_LGUI,
+                                                                   KC_DELT, KC_HOME,
+                                                                            KC_END,
+                                                     CTL_T(KC_NO), KC_SPC,  KC_LGUI,
         // right hand
-        KC_RGHT,      KC_6,         KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_SPC,     KC_Y,         KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-                      KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        MO(SYMB),     KC_N,         KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                    KC_RGUI, KC_LBRC, KC_RBRC, KC_GRV,  TG(SYMB),
+        KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_SPC,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        MO(SYMB),     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                               KC_RGUI, KC_GRV,  KC_LBRC, KC_RBRC, TG(SYMB),
         KC_PGUP,      KC_BSPC,
         KC_PGDN,
-        KC_RGUI,      KC_SPC,       KC_ENT
+        KC_RGUI,      KC_SPC,  KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
