0efc79b6dd3fb682c3205d6886a193afe98adb3c
couhie <kohei.hieda@gmail.com>
2017-05-03 08:45:44 +0900

    記号系のキーマップを変更

--- keyboards/ergodox/keymaps/couhie/keymap.c
+++ keyboards/ergodox/keymaps/couhie/keymap.c
@@ -18,15 +18,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Grv    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | Space|           | Space|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
+ * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
  * |--------+------+------+------+------+------| ~L1  |           | ~L1  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L2  |  Esc | Ctrl | Alt  | LGui |                                       | RGui |  Grv |   [  |   ]  |   L1 |
+ *   | ~L2  |  Esc | Ctrl | Alt  | LGui |                                       | RGui |   *  |   =  |   +  |   L1 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Del  | Home |       | PgUp | BkSp   |
@@ -40,34 +40,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,       KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_LEFT,
-        KC_TAB,       KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    KC_SPC,
-        CTL_T(KC_NO), KC_A,   KC_S,         KC_D,    KC_F,         KC_G,
-        KC_LSFT,      KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    MO(SYMB),
-        MO(MDIA),     KC_ESC, CTL_T(KC_NO), KC_LALT, KC_LGUI,
-                                                                   KC_DELT, KC_HOME,
-                                                                            KC_END,
-                                                     CTL_T(KC_NO), KC_SPC,  KC_LGUI,
+        KC_GRV,        KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_LEFT,
+        KC_TAB,        KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    KC_SPC,
+        CTL_T(KC_ESC), KC_A,   KC_S,         KC_D,    KC_F,         KC_G,
+        KC_LSFT,       KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    MO(SYMB),
+        MO(MDIA),      KC_ESC, CTL_T(KC_NO), KC_LALT, KC_LGUI,
+                                                                    KC_DELT, KC_HOME,
+                                                                             KC_END,
+                                                      CTL_T(KC_NO), KC_SPC,  KC_LGUI,
         // right hand
-        KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_SPC,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        MO(SYMB),     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                               KC_RGUI, KC_GRV,  KC_LBRC, KC_RBRC, TG(SYMB),
-        KC_PGUP,      KC_BSPC,
+        KC_RGHT,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
+        KC_SPC,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
+                  KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+        MO(SYMB), KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                           KC_RGUI, KC_ASTR, KC_EQL, KC_PLUS,  TG(SYMB),
+        KC_PGUP,  KC_BSPC,
         KC_PGDN,
-        KC_RGUI,      KC_SPC,  KC_ENT
+        KC_RGUI,  KC_SPC,  KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |Version  |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      |      |   7  |   8  |   9  |   /  |        |
+ * |         |   !  |   @  |   {  |   }  |   &  |      |           |      |      |   7  |   8  |   9  |   /  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |------|           |------|      |   4  |   5  |   6  |   *  |        |
+ * |         |   #  |   $  |   (  |   )  |   `  |------|           |------|      |   4  |   5  |   6  |   *  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      |      |   1  |   2  |   3  |   -  |        |
+ * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |      |   1  |   2  |   3  |   -  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | EPRM  |      |      |      |      |                                       |   0  |    . |   =  |   +  |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        VRSN,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR, KC_TRNS,
+       KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,
+       KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,
        EPRM,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                     KC_TRNS, KC_TRNS,
                                                              KC_TRNS,
