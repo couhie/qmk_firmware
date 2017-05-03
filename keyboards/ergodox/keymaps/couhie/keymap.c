637d2d71126b9510de0dcd03b3b7026ce6a19a6b
couhie <kohei.hieda@gmail.com>
2017-05-03 09:11:48 +0900

    スペースのキーマップを変更

--- keyboards/ergodox/keymaps/couhie/keymap.c
+++ keyboards/ergodox/keymaps/couhie/keymap.c
@@ -20,10 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Grv    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | Space|           | Space|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | ~L1  |           | ~L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
- * |--------+------+------+------+------+------| ~L1  |           | ~L1  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Space|           | Space|------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | ~L2  |  Esc | Ctrl | Alt  | LGui |                                       | RGui |   *  |   =  |   +  |   L1 |
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | Del  | Home |       | PgUp | BkSp   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | End  |       | PgDn |        |      |
- *                                 | Ctrl |Space |------|       |------| Space  |Enter |
- *                                 |      |      | LGui |       | RGui |        |      |
+ *                                 | Ctrl |Space |------|       |------| RGui   |Enter |
+ *                                 |      |      | LGui |       | Space|        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -41,22 +41,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_GRV,        KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_LEFT,
-        KC_TAB,        KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    KC_SPC,
+        KC_TAB,        KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    MO(SYMB),
         CTL_T(KC_ESC), KC_A,   KC_S,         KC_D,    KC_F,         KC_G,
-        KC_LSFT,       KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    MO(SYMB),
+        KC_LSFT,       KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    KC_SPC,
         MO(MDIA),      KC_ESC, CTL_T(KC_NO), KC_LALT, KC_LGUI,
                                                                     KC_DELT, KC_HOME,
                                                                              KC_END,
                                                       CTL_T(KC_NO), KC_SPC,  KC_LGUI,
         // right hand
         KC_RGHT,  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-        KC_SPC,   KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
+        MO(SYMB), KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLS,
                   KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        MO(SYMB), KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_SPC,   KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                            KC_RGUI, KC_ASTR, KC_EQL, KC_PLUS,  TG(SYMB),
         KC_PGUP,  KC_BSPC,
         KC_PGDN,
-        KC_RGUI,  KC_SPC,  KC_ENT
+        KC_SPC,   KC_RGUI, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
