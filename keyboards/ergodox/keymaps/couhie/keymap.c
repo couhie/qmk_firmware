aa12920731a51e5a46f1afdbaa76a10af5b269f9
couhie <kohei.hieda@gmail.com>
2017-05-25 13:16:21 +0900

    Enterキーの位置調整

--- keyboards/ergodox/keymaps/couhie/keymap.c
+++ keyboards/ergodox/keymaps/couhie/keymap.c
@@ -1,3 +1,5 @@
+// make keyboard=ergodox keymap=couhie
+
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
@@ -18,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Grv    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | Grv    |   1  |   2  |   3  |   4  |   5  | Esc  |           | Enter|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  =   |           |  -   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -40,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,        KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_LEFT,
+        KC_GRV,        KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_ESC,
         KC_TAB,        KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    KC_EQL,
         CTL_T(KC_ESC), KC_A,   KC_S,         KC_D,    KC_F,         KC_G,
         KC_LSFT,       KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    KC_SPC,
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                              KC_END,
                                                       CTL_T(KC_NO), KC_SPC,  KC_LGUI,
         // right hand
-        KC_RGHT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
