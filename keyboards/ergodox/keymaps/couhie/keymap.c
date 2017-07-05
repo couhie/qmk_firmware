ce58a629171b4c67e0fd32719c6d9ad557e37fc8
couhie <kohei.hieda@gmail.com>
2017-07-05 17:20:02 +0900

    ctrlキーが押しっぱなしにならないよう変更

--- keyboards/ergodox/keymaps/couhie/keymap.c
+++ keyboards/ergodox/keymaps/couhie/keymap.c
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,        KC_1,   KC_2,         KC_3,    KC_4,         KC_5,    KC_ESC,
-        KC_TAB,        KC_Q,   KC_W,         KC_E,    KC_R,         KC_T,    KC_EQL,
-        CTL_T(KC_ESC), KC_A,   KC_S,         KC_D,    KC_F,         KC_G,
-        KC_LSFT,       KC_Z,   KC_X,         KC_C,    KC_V,         KC_B,    KC_SPC,
-        MO(MDIA),      KC_ESC, CTL_T(KC_NO), KC_LALT, KC_LGUI,
-                                                                    KC_DELT, KC_HOME,
-                                                                             KC_END,
-                                                      CTL_T(KC_NO), KC_SPC,  KC_LGUI,
+        KC_GRV,        KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC,
+        KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL,
+        CTL_T(KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LSFT,       KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,
+        MO(MDIA),      KC_ESC, KC_LCTL, KC_LALT, KC_LGUI,
+                                                          KC_DELT, KC_HOME,
+                                                                   KC_END,
+                                                 KC_LCTL, KC_SPC,  KC_LGUI,
         // right hand
         KC_ENT,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
